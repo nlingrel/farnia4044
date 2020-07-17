1fd196ac627b82020d61875f373e7d0d9d49a326
Nicholas Lingrel <nicholas.lingrel@gmail.com>
2020-07-17 15:42:33 -0500

    "Render random orbits on system choices"

--- farnia4044/src/components/System.jsx
+++ farnia4044/src/components/System.jsx
@@ -1,35 +1,60 @@
-import React, { useContext } from "react";
-import HudIcon from "./HudIcon";
+import React from "react";
 
 export default function System(props) {
   const getRotationAngle = () => {
     return Math.floor(Math.random() * 360);
   };
-  const angle1 = getRotationAngle();
-  const angle2 = getRotationAngle();
+  const getRotationString = (angle) => {
+    const angl = angle ? angle : getRotationAngle();
+    return `rotate(${angl}, ${ellipseCX}, ${ellipseCY} )`;
+  };
+  const getRxRy = () => {
+    const xfloor = 12;
+    const xceil = 18;
+    const yfloor = 17;
+    const yceil = 26;
+    let rx = xfloor + Math.floor(Math.random() * (xceil - xfloor));
+    let ry = yfloor + Math.floor(Math.random() * (yceil - yfloor));
+    return { rx: `${rx}`, ry: `${ry}` };
+  };
+  const amnt = Math.floor(Math.random() * 3) + 1;
+
   const ellipseCX = "40";
   const ellipseCY = "40";
-  const rotate1 = `rotate(${angle1}, ${ellipseCX}, ${ellipseCY} )`;
-  const rotate2 = `rotate(${angle2}, ${ellipseCX}, ${ellipseCY} )`;
+
+  const makeEllipses = (amnt) => {
+    let ellipses = [];
+    let rxry = getRxRy();
+    let startRotAngle = getRotationAngle();
+
+    for (let i = 0; i < amnt; i++) {
+      ellipses.push(
+        <ellipse
+          cx={ellipseCX}
+          cy={ellipseCY}
+          rx={rxry.rx}
+          ry={rxry.ry}
+          transform={getRotationString(startRotAngle + (i + 1) * startRotAngle)}
+          value={props.value}
+          style={{
+            fillOpacity: "0",
+            strokeWidth: "0",
+            strokeWidth: "1",
+            stroke: "white",
+            zIndex: "2",
+          }}
+        />
+      );
+    }
+    return ellipses;
+  };
+  const ellipses = makeEllipses(amnt);
+
   return (
     <>
       <div className="d-flex justify-content-center" value={props.value}>
         <svg height="80" width="80">
-          <ellipse
-            cx="40"
-            cy="40"
-            rx="24"
-            ry="20"
-            transform={rotate1}
-            value={props.value}
-            style={{
-              fillOpacity: "0",
-              strokeWidth: "0",
-              strokeWidth: "1",
-              stroke: "white",
-              zIndex: "2",
-            }}
-          />
+          {ellipses}
           <text
             textLength="18"
             lengthAdjust="spacingAndGlyphs"
@@ -40,36 +65,7 @@ export default function System(props) {
           >
             {props.symbol}
           </text>
-
-          <ellipse
-            cx="40"
-            cy="40"
-            rx="28"
-            ry="14"
-            transform={rotate2}
-            style={{
-              fillOpacity: "0",
-              strokeWidth: "0",
-              strokeWidth: "1",
-              stroke: "white",
-              zIndex: "2",
-            }}
-            value={props.value}
-          />
-          {/* <ellipse
-            cx="40"
-            cy="40"
-            rx="18"
-            ry="18"
-            style={{ fillOpacity: "0", strokeWidth: "0", opacity: ".9" }}
-          /> */}
         </svg>
-
-        {/* <HudIcon
-          symbol={props.symbol}
-          value={props.value}
-          color={props.color}
-        /> */}
       </div>
     </>
   );
