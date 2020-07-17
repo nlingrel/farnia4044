20396af7f650214a1f49eeaa5917a6c89586a326
Nicholas Lingrel <nicholas.lingrel@gmail.com>
2020-07-16 19:01:15 -0500

    "Add build folder for deployment"

---
+++ farnia4044/build/precache-manifest.6986be6b9ad44f7efb6ba759dc910115.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa5df1698fd73a3d54fceda2f6901289",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e99d47028c6ed624b7e",
+    "url": "/static/css/main.e72f7f81.chunk.css"
+  },
+  {
+    "revision": "fcf7937c9117905db0fc",
+    "url": "/static/js/2.5ad82449.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5ad82449.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e99d47028c6ed624b7e",
+    "url": "/static/js/main.10e9e378.chunk.js"
+  },
+  {
+    "revision": "e87720d6c0cf3b8bb1f2",
+    "url": "/static/js/runtime-main.be5e99ab.js"
+  }
+]);
